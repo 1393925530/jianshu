b89ff876f163e3244ff5a2a8f6313f5be31ac108
1393925530 <zzc1393925530@gmail.com>
2019-02-28 21:34:10 +0800

    使用styled-components完成简书header布局

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { GlobalStyled } from './style.js';
+import Header from './common/header';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        hello world
-        <GlobalStyled />
-      </div>
+      <Header />
     );
   }
 }
