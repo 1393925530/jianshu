0681fdc0b7fee2fd87e75bb60a8535f9c435677b
1393925530 <zzc1393925530@gmail.com>
2019-03-01 21:20:47 +0800

    actionCreators和constants的拆分

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,16 +1,18 @@
+import * as constants from './constants';
+
 const defaultStore = {
     focused: false,
 };
 
 export default (state = defaultStore, action) => {
-    if (action.type === "search_focus") {
+    if (action.type === constants.SEARCH_FOCUS) {
         // const newState = JSON.parse(JSON.stringify(state));
         // newState.focused = true;
         return {
             focused: true,
         };
     }
-    if (action.type === "search_blur") {
+    if (action.type === constants.SEARCH_BLUR) {
         // const newState = JSON.parse(JSON.stringify(state));
         // newState.focused = true;
         return {
