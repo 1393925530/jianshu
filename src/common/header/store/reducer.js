26208c288e21e44cac2acff8f438491a19b7141a
1393925530 <zzc1393925530@gmail.com>
2019-03-02 00:22:32 +0800

    搜索框换一批功能实现

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -3,17 +3,29 @@ import { fromJS } from 'immutable';
 
 const defaultStore = fromJS({
     focused: false,
-    list: []
+    mouseIn: false,
+    list: [],
+    page: 1,
+    totalPage: 1
 });
 
 export default (state = defaultStore, action) => {
     switch(action.type) {
         case constants.SEARCH_FOCUS :
-        return state.set('focused', true);
+            return state.set('focused', true);
         case constants.SEARCH_BLUR :
-        return state.set('focused', false);
+            return state.set('focused', false);
         case constants.CHANGE_LIST :
-        return state.set('list', action.data);
+            return state.merge({
+                list: action.data,
+                totalPage: action.totalPage
+            });
+        case constants.Mouse_ENTER:
+            return state.set('mouseIn', true);
+        case constants.Mouse_LEAVE:
+            return state.set('mouseIn', false);
+        case constants.CHANGE_PAGE:
+            return state.set('page', action.page);
         default :
         return state;
     }
