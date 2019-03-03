3bae3c23d2ecda2fd8b2a846269db20041c644f3
1393925530 <zzc1393925530@gmail.com>
2019-03-03 23:08:11 +0800

    首页组件的拆分

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { GlobalStyled } from './style.js';
 import { IconFontStyled } from './statics/iconfont/iconfont';
 import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import store from './store';
 
 class App extends Component {
@@ -15,8 +17,8 @@ class App extends Component {
             <Header />
               <BrowserRouter>
                 <div>
-                  <Route path='/' exact render={()=><div>home</div>}></Route>
-                  <Route path='/detail' exact render={()=><div>detail</div>}></Route>
+                  <Route path='/' exact component={Home}></Route>
+                  <Route path='/detail' exact component={Detail}></Route>
                 </div>
               </BrowserRouter>
             <IconFontStyled />
---
+++ src/pages/home/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import { 
+    HomeWrapper, 
+    HomeLeft, 
+    HomeRight
+ } from './style';
+
+class Home extends Component {
+    render() {
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className="banner-image" src="//upload.jianshu.io/admin_banners/web_images/4615/62909ce23863ac5f6a1454c7ba407b85af0a17db.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
+                    <Topic />
+                    <List />
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+            </HomeWrapper>
+        )
+    }
+}
+
+export default Home;
