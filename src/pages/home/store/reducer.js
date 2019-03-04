83eabec6ba5b07260689c174f655528e7e1979dd
1393925530 <zzc1393925530@gmail.com>
2019-03-04 21:45:12 +0800

    首页异步数据的获取显示

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,47 +1,19 @@
 import { fromJS } from 'immutable';
 
 const defaultStore = fromJS({
-    topicList: [{
-        id: 1,
-        title: "社会热点",
-        imgUrl: "//upload-images.jianshu.io/upload_images/15874461-43b02ba37025dc2e?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
-    }, {
-        id: 2,
-        title: "手绘",
-        imgUrl: "//upload-images.jianshu.io/upload_images/16222142-2a6f833697804389.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
-    }],
-    articleList: [{
-        id: 1,
-        title: "刘备死后，诸葛亮为什么屡战屡败？姜维临终前说了8个字！",
-        desc: "不管在任何年代，要想让国家繁荣昌盛，一定离不开的就是人才。三国时期可以称得上是人才辈出。 但是有的人才并不愿意掺进这纷杂的乱世，但是无奈于即使自...",
-        imgUrl: "//upload-images.jianshu.io/upload_images/16222142-2a6f833697804389.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
-    }, {
-        id: 2,
-        title: "刘备死后，诸葛亮为什么屡战屡败？姜维临终前说了8个字！",
-        desc: "不管在任何年代，要想让国家繁荣昌盛，一定离不开的就是人才。三国时期可以称得上是人才辈出。 但是有的人才并不愿意掺进这纷杂的乱世，但是无奈于即使自...",
-        imgUrl: "//upload-images.jianshu.io/upload_images/16222142-2a6f833697804389.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
-    }, {
-        id: 3,
-        title: "刘备死后，诸葛亮为什么屡战屡败？姜维临终前说了8个字！",
-        desc: "不管在任何年代，要想让国家繁荣昌盛，一定离不开的就是人才。三国时期可以称得上是人才辈出。 但是有的人才并不愿意掺进这纷杂的乱世，但是无奈于即使自...",
-        imgUrl: "//upload-images.jianshu.io/upload_images/16222142-2a6f833697804389.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
-    }, {
-        id: 4,
-        title: "刘备死后，诸葛亮为什么屡战屡败？姜维临终前说了8个字！",
-        desc: "不管在任何年代，要想让国家繁荣昌盛，一定离不开的就是人才。三国时期可以称得上是人才辈出。 但是有的人才并不愿意掺进这纷杂的乱世，但是无奈于即使自...",
-        imgUrl: "//upload-images.jianshu.io/upload_images/16222142-2a6f833697804389.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240",
-    }],
-    recomendList: [{
-        id: 1,
-        imgUrl: "http://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png",
-    }, {
-        id: 2,
-        imgUrl: "http://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png",
-    }]
+    topicList: [],
+    articleList: [],
+    recomendList: []
 });
 
 export default (state = defaultStore, action) => {
     switch (action.type) {
+        case 'change_home_data':
+            return state.merge({
+                'topicList': fromJS(action.topicList),
+                'articleList': fromJS(action.articleList),
+                'recomendList': fromJS(action.recomendList),
+            })
         default:
             return state;
     }
