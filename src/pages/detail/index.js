8eae0e48a1f8beea792ee848047f61ef810c6378
1393925530 <zzc1393925530@gmail.com>
2019-03-05 19:53:10 +0800

    使用redux管理详情页面的数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component {
@@ -6,20 +7,20 @@ class Detail extends Component {
         return (
             <DetailWrapper>
                 <Header>
-                    我离开你，不是不喜欢你了，而是
+                    {
+                        this.props.title
+                    }
                 </Header>
-                <Content>
-                    <img 
-                    alt=""
-                    src="https://upload-images.jianshu.io/upload_images/15069078-100d4bdb63b24ea0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240" />
-                    <p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p>
-                    <p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p>
-                    <p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p>
-                    <p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p>
-                </Content>
+                <Content 
+                dangerouslySetInnerHTML={{__html: this.props.content}} />
             </DetailWrapper>
         )
     }
 }
 
-export default Detail;
+const mapState = (state) => ({
+    title: state.getIn(["detail", "title"]),
+    content: state.getIn(["detail", "content"]),
+})
+
+export default connect(mapState, null)(Detail);
---
+++ src/pages/detail/store/reducer.js
@@ -0,0 +1,14 @@
+import { fromJS } from 'immutable';
+import * as constants from './constants';
+
+const defaultState = fromJS({
+    title: '我离开你，不是不喜欢你了，而是',
+    content: '<img alt="" src="https://upload-images.jianshu.io/upload_images/15069078-100d4bdb63b24ea0.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240" /><p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p><p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p><p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p><p>这句话说在前面，是因为问题问的是“想让人恋爱的女生”，就是能够激发起男人的最原始的雄性欲望的女生，其中包含了<b>性欲，保护欲，征服欲，陪伴，欣赏和渴望。</b></p>'
+});
+
+export default (state = defaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
